c65e1103d053e95ff473ac7782cf275c1988881b
Sklyar Vitaliy <it.sklyarv@yandex.ru>
2023-09-09 17:00:05 +0300

    fix problem with mongo

--- base-install.sh
+++ base-install.sh
@@ -8,8 +8,8 @@ echo 'deb https://www.ui.com/downloads/unifi/debian stable ubiquiti' | tee /etc/
 
 
 #Install MongoDB
-curl -fsSL https://pgp.mongodb.com/server-7.0.asc | gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+curl -fsSL https://pgp.mongodb.com/server-4.4.asc | gpg -o /usr/share/keyrings/mongodb-server-4.4.gpg --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-4.4.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 apt update
 
